47e9eda8ef038f614bfd9bf2272fdea2519f9a2f
funera1 <funeral7776@gmail.com>
2022-08-10 21:09:39 +0900

    update: CLIツールとして使えるようにcobraを導入

--- main.go
+++ main.go
@@ -1,37 +1,11 @@
-// 使い方: go doc $FILEPATH | go run main.go
-//
-// 機能: go docで出力したコメント内部のソースコードに対してフォーマットをかける
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"go/doc/comment"
-	"go/format"
-	"io/ioutil"
-	"os"
-)
+import "github.com/funera1/gofmtal/cmd"
 
 func main() {
-
-    // cmntにはgo doc $FILEPATHの出力結果が入力されることを期待する
-	var cmnt string
-	bytes, err := ioutil.ReadAll(os.Stdin)
-    if err != nil {
-        panic(err)
-    }
-    cmnt = string(bytes)
-
-    // コメントのソースコードに対しフォーマットをかける
-	var p comment.Parser
-	doc := p.Parse(cmnt)
-	for _, c := range doc.Content {
-		switch c := c.(type) {
-		case *comment.Code:
-			src, err := format.Source([]byte(c.Text))
-			if err == nil {
-				c.Text = string(src)
-			}
-		}
-	}
-	var pr comment.Printer
-	os.Stdout.Write(pr.Comment(doc))
+	cmd.Execute()
 }
