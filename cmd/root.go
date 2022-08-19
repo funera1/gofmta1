b1c86de842dc43eb96acc17cd350f499fe377006
funera1 <funeral7776@gmail.com>
2022-08-19 16:51:12 +0900

    あとからgofmtをかけるのでコード全部に対してFormatCodeを掛けて良い

--- cmd/root.go
+++ cmd/root.go
@@ -5,20 +5,23 @@ package cmd
 
 import (
 	"fmt"
-	"go/ast"
 	"go/doc/comment"
 	"go/format"
-	"go/parser"
-	"go/token"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-func FormatCode(cmnt string) string {
-	// コメントのソースコードに対しフォーマットをかける
+// コードに対しフォーマットを掛けた文字列を返す
+func FormatCode(filename string) (formattedCode string, err error) {
+	// ファイルの中身を取得
+	b, err := ioutil.ReadFile(filename)
+	code := string(b)
+
+	// コード中のソースコードを抜き出しフォーマットをかける
 	var p comment.Parser
-	doc := p.Parse(cmnt)
+	doc := p.Parse(code)
 	for _, c := range doc.Content {
 		switch c := c.(type) {
 		case *comment.Code:
@@ -29,64 +32,9 @@ func FormatCode(cmnt string) string {
 		}
 	}
 	var pr comment.Printer
-	return string(pr.Comment(doc))
-}
-
-// とりあえず一つのファイルについてASTを返す
-func GetAst(filename string) (f *ast.File, err error) {
-	fset := token.NewFileSet()
-	f, err = parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	b, err = format.Source(pr.Comment(doc))
+	formattedCode = string(b)
 	return
-}
-
-type CodeBlock struct {
-	IsComment bool
-	Text      string
-}
-
-func DevideIntoCommentAndNonComment(code string, ast *ast.File) []CodeBlock {
-	// コメントの位置がわかれば良さそう
-	var blocks []string
-	var devidePos []int = []int{0}
-	isCommentMap := map[int]bool{}
-	// 区切る位置のリストを取得
-	for _, cmntGrp := range ast.Comments {
-		for _, cmnt := range cmntGrp.List {
-			pos := int(cmnt.Slash) - 1
-			offset := len(cmnt.Text)
-			devidePos = append(devidePos, pos)
-			devidePos = append(devidePos, pos+offset)
-
-			isCommentMap[pos] = true
-		}
-	}
-	// 最後の位置も分割位置として含めておくことで実装が楽になる
-	devidePos = append(devidePos, len([]rune(code)))
-
-	// devidePosに従ってcodeを分割する
-	for i := 0; i < len(devidePos)-1; i++ {
-		start := devidePos[i]
-		end := devidePos[i+1]
-		blocks = append(blocks, string([]rune(code)[start:end]))
-	}
-
-	// blocks[i]がコメントかどうかの値をつける
-	var codeBlocks []CodeBlock
-	for i := 0; i < len(devidePos)-1; i++ {
-		pos := devidePos[i]
-		if _, ok := isCommentMap[pos]; ok {
-			codeBlocks = append(codeBlocks, CodeBlock{
-				IsComment: true,
-				Text:      blocks[i],
-			})
-		} else {
-			codeBlocks = append(codeBlocks, CodeBlock{
-				IsComment: false,
-				Text:      blocks[i],
-			})
-		}
-	}
-	return codeBlocks
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -96,30 +44,13 @@ var rootCmd = &cobra.Command{
 	Long:  "",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		target := args[0]
-
-		ast, err := GetAst(target)
+		// TODO: 複数ファイルやディレクトリ指定したときの対応
+		targetfile := args[0]
 		if err != nil {
-			return err
+			return
 		}
 
-		// codeについてコメントとコメントでわけてブロックにする
-		codeBlocks := DevideIntoCommentAndNonComment(target, ast)
-
-		// コメント部分についてのみFormatCodeを適用する
-		for i, cb := range codeBlocks {
-			if cb.IsComment {
-				codeBlocks[i].Text = FormatCode(cb.Text)
-			}
-		}
-
-		// codeBlocksをもとに戻す
-		var formattedCode string
-		for _, cb := range codeBlocks {
-			formattedCode += cb.Text
-		}
-
-		// TODO: 出力先を指定できるようにする
+		formattedCode, err := FormatCode(targetfile)
 		fmt.Println(formattedCode)
 		return
 	},
