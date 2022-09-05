dab55458d3291c7d2289a2d570951ed8064e40a3
funera1 <funeral7776@gmail.com>
2022-09-06 01:47:47 +0900

    clean

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"bytes"
 	"flag"
 	"os"
 	"path/filepath"
@@ -19,46 +18,6 @@ func init() {
 	flag.BoolVar(&flagUpdate, "update", false, "update golden files")
 }
 
-func runTest(t *testing.T, in, out string) {
-	_, err := os.Lstat(in)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	tmp, err := processFile(in)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	got := []byte(tmp)
-
-	want, err := os.ReadFile(out)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if !bytes.Equal(got, want) {
-		// if *update {
-		// 	if in != out {
-		// 		if err := os.WriteFile(out, got, 0666); err != nil {
-		// 			t.Error(err)
-		// 		}
-		// 		return
-		// 	}
-		// in == out: don't accidentally destroy input
-		// t.Errorf("WARNING: -update did not rewrite input file %s", in)
-		// }
-
-		// t.Errorf("(gofmt %s) != %s (see %s.gofmt)\n%s", in, out, in,
-		// 	diff.Diff("expected", want, "got", got))
-		// if err := os.WriteFile(in+".gofmt", got, 0666); err != nil {
-		// 	t.Error(err)
-		// }
-	}
-}
-
 func Test(t *testing.T) {
 	// determine input files
 	match, err := filepath.Glob("../testdata/*.input")
@@ -73,11 +32,6 @@ func Test(t *testing.T) {
 			if strings.HasSuffix(in, ".input") {
 				out = in[:len(in)-len(".input")] + ".golden"
 			}
-			// runTest(t, in, out)
-			// if in != out && !t.Failed() {
-			// 	// Check idempotence.
-			// 	runTest(t, out, out)
-			// }
 
 			got, err := processFile(in)
 			if err != nil {
